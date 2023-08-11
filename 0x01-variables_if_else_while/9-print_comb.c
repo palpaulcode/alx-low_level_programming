45c5df21142833d92d930b372130a62994ba7f29
Paul Nzioki <palpaul49@gmail.com>
2023-08-12 02:49:59 +0300

    Add file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+			putchar(44);
+		putchar(32);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
