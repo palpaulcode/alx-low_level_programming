bca9021f9ce8735854a98d23c97d075b474a1d84
Paul Nzioki <palpaul49@gmail.com>
2023-08-11 03:18:50 +0300

    Modify file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
 /**
  * main - Entry point
  *
- * Description: Prints a string of characters followed by newline to standard error
+ * Description: Prints a string of characters followed 
+ * by newline to standard error
  *
  * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
