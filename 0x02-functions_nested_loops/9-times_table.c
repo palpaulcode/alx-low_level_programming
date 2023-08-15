f3073b3274393ae9951e5249d0828c1395606703
Paul Nzioki <palpaul49@gmail.com>
2023-08-16 00:32:34 +0300

    Update file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,24 +15,25 @@ void times_table(void)
 	l = 0;
 	while (l <= 9)
 	{
-		c = 0;
+		_putchar('0');
+
+		c = 1;
 		while (c < 10)
 		{
-			r = c * l;			/* result from multiplication */
+		 	r = c * l;		/* result from multiplication */
+
+			_putchar(44);
+			_putchar(32);
 
 			if (r < 9)
-				_putchar(' ');		/* append a space before values less than 10 */
+			       _putchar(' ');	/* append a space before values less than 10 */
 			else
-				_putchar((r / 10) + '0');
+			       _putchar((r / 10) + '0');
 
 			_putchar((r % 10) + '0');
 
-			if (c != 9)			/* make sure no comma and space at end of each line */
-			{
-				_putchar(44);		/* comma */
-				_putchar(32);		/* space */
-			}
 			c++;
+			
 		}
 		_putchar('\n');
 		l++;
