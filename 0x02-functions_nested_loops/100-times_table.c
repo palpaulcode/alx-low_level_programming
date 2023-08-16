a6a13a5aabf40f968a2da346183239824fc74005
Paul Nzioki <palpaul49@gmail.com>
2023-08-16 04:15:31 +0300

    Update file 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,56 +1,48 @@
 #include "main.h"
-
 /**
  * print_times_table - prints times table of a given  number starting with 0
  * @n: the number whose times table is to be printed
  *
  * Return: returns no value; has void return type
  */
-
 void print_times_table(int n)
 {
-	int cnt;	/* counter for the first loop */
-	int lp;		/* counter for the second loop */
-	int res;	/* multiplication result */
-
+	int cnt, lp, res;	/* cnt-counter for loop1 np-counter
+				 * for loop2 res-multiplication result
+				 */
 	cnt = 0;
-
 	if ((n > 0) && (n < 15))
 	{
-	while (cnt <= n)
-	{
-		_putchar('0');
-
-		lp = 1;
-		while (lp <= n)
+		while (cnt <= n)
 		{
-			res = cnt * lp;		/* result for multiplication */
-
-			_putchar(44);
-			_putchar(32);
-
-			if (res < 10)
+			_putchar('0');
+			lp = 1;
+			while (lp <= n)
 			{
-				_putchar(' ');	/* append a space to values less than 10 */
-				_putchar(' ');
-			}
-			else if (res >= 10 && res < 100)
-			{
+				res = cnt * lp;		/* result for multiplication */
+				_putchar(44);
+				_putchar(32);
+				if (res < 10)
+				{
+					_putchar(' ');	/* append a space to values less than 10 */
+					_putchar(' ');
+				}
+				else if (res >= 10 && res < 100)
+				{
 				_putchar(' ');
 				_putchar((res / 10) + '0');
+				}
+				else
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+				}
+				_putchar((res % 10) + '0');
+				lp++;
 			}
-			else
-			{
-				_putchar((res / 100) + '0');
-				_putchar((res / 10) % 10 + '0');
-			}
-
-			_putchar((res % 10) + '0');
-			lp++;
+			_putchar('\n');
+			cnt++;
 		}
-		_putchar('\n');
-		cnt++;
-	}
 	}
 
 }
