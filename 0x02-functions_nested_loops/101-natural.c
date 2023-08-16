1a58bcc1b750a4cdbcec986919cb53235120157b
Paul Nzioki <palpaul49@gmail.com>
2023-08-16 04:33:08 +0300

    Add file 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * sum_multiples_of_3_and_5 - find sum of multiples of 3 and 5 below 1024
+ * 
+ * Return: has void return type
+ */
+
+void sum_multiples_of_3_and_5(void)
+{
+	int num;	/* hold value of natural number; they start from 1 */
+	int sum = 0;	/* sum of multiples of three and five. Initialized to 0 */
+
+	for (num = 1; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+
+	printf("%d\n", sum);
+
+}
