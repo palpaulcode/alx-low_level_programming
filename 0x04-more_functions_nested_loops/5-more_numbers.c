40c96d73f79c9f19cc1e08a4733383eba61acc16
Paul Nzioki <palpaul49@gmail.com>
2023-08-18 00:50:50 +0300

    Update file 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9.
- * skips numbers 2 and 4
+ * more_numbers - prints numbers from 0 to 14 ten times.
  *
  * Return: has void return type; no return type
  */
