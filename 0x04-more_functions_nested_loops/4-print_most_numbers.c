6c97979fb0b7eac7e476c98d563b866011d1dca5
Paul Nzioki <palpaul49@gmail.com>
2023-08-17 23:59:30 +0300

    Add file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9. 
+ * skips numbers 2 and 4
+ *
+ * Return: has void return type; no return type
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	num = 48;
+
+	while (num <= 57)
+	{
+		if (num == 50 || num == 52)
+			continue;
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
