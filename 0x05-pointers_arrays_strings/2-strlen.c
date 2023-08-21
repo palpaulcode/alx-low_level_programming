49fc5e162f9c9996e68f6e6ddb72e9e46df17d31
Paul Nzioki <palpaul49@gmail.com>
2023-08-21 17:38:25 +0300

    Update file 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 /**
- * main - 
+ * _strlen - returns the length of a string
+ * @s: the string to return length of
  *
- *
+ * Return: returns the length of the string
  */
 int _strlen(char *s)
 {
