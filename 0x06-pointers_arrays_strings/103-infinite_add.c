d5d3137fb31581e64e13188020ee8e08739a145d
Paul Nzioki <palpaul49@gmail.com>
2023-08-25 01:01:09 +0300

    Update file 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,9 @@
 /**
- * infinte_add - adds two numbers
+ * infinite_add - adds two numbers
  * @n1: the first number
  * @n2: the second number
  * @r: buffer to store the result
  * @size_r: the size of the buffer
- *
  * Return: returns the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -13,55 +12,37 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (n1[i] != '\0')
 		i++;
-
 	while (n2[j] != '\0')
 		j++;
-
 	if (i > size_r || j > size_r)
 		return (0);
-
 	i--; /* i -= 1 */
 	j--; /* j -= 1 */
-	/* m = 0; */
-	/* k = 0; */
 
-	while (k < size_r - 1)
+	for (; k < size_r - 1; i--, j--, k++)
 	{
-		/* n = m; */
+		n = m;
 		if (i >= 0)
 			n += n1[i] - '0';
-		
+
 		if (j >= 0)
 			n += n2[j] - '0';
 
 		if (i < 0 && j < 0 && n == 0)
 			break;
-		
+
 		m = n / 10;
 		r[k] = n % 10 + '0';
-
-		i--;
-		j--;
-		k++;
 	}
-	
 	r[k] = '\0';
-
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-
 	k -= 1;
-	/* l = 0; */
-	while (l < k)
+	for (; l < k; k--, l++)
 	{
 		m = r[k];
 		r[k] = r[l];
 		r[l] = m;
-
-		k--;
-		l++;
 	}
-
 	return (r);
-
 }
