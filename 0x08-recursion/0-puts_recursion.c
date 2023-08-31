732b463090b971514009550478e3c64332289487
Paul Nzioki <palpaul49@gmail.com>
2023-08-31 03:04:35 +0300

    Add file 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (strlen(s) == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+       	_puts_recursion(s + 1);
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,7 @@
+# 0x08 Recursion
+This repository contains coding tasks on recursion in C programming in the ALX_SE course
+
+## File `0-puts_recursion.c`
+Contains a function that prints a string followed by a new line
+
+
