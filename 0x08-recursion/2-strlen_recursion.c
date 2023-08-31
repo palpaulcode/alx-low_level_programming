47e7e7b3cca8023196798970da5d35d2718e9fd5
Paul Nzioki <palpaul49@gmail.com>
2023-08-31 03:39:43 +0300

    Add file 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to get length of
+ *
+ * Return: integer length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (strlen(s) == 0)
+		return 0;
+	return (1 + _strlen_recursion(s + 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,4 +7,7 @@ Contains a function that prints a string followed by a new line
 ## File `1-print_rev_recursion.c`
 Print a string in reverse
 
+## File `2-strlen_recursion.c`
+Returns the length of a string
+
 
