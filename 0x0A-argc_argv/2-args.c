4a9ac078457981d211a5b21c76464ad297346dc5
Paul Nzioki <palpaul49@gmail.com>
2023-09-02 02:46:53 +0300

    Add file 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc: contains number of arguments to main
+ * @argv: pointer to array of pointers of the arguments
+ * passed to the program when it is executed
+ *
+ * Return: returns 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while(i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
