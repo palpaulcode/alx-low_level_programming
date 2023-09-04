74f489296ac34f037ba40e57b131079dc0811ac7
Paul Nzioki <palpaul49@gmail.com>
2023-09-05 00:03:50 +0300

    Update file 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,13 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	if (size == 0)
-		return (NULL);
-
 	char *arr = malloc(sizeof(c) * size);
 
 	while (i < size)
 	{
 		arr[i] = c;
+		if (size == NULL)
+			return (NULL);
 		i++;
 	}
 	return (arr);
