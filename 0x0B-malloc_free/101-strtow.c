0c0dcd8d20a4d636df6d9628fb53bb1af177db6d
Paul Nzioki <palpaul49@gmail.com>
2023-09-05 16:02:33 +0300

    Update file 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,6 +9,9 @@ int str_len(char *str)
 {
 	int l = 0; /* hold string length */
 
+	if (str == ' ') /* if empty string */
+		return (NULL);
+
 	if (str != NULL)
 	{
 		while (str[l])
@@ -25,6 +28,8 @@ int str_len(char *str)
 int no_of_words(char *str)
 {
 	int i = 0, w_count = 0;
+
+	/* here */
 
 	while (i <= str_len(str))
 	{
@@ -55,6 +60,8 @@ int no_of_words(char *str)
 char **str_words(char **w_arr, char *str, int w_count)
 {
 	int i, j = 0, w_size = 0, tmp = 0;
+
+	/* here */
 
 	if (w_arr != NULL)
 	{
