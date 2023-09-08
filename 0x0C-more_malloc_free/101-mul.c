df6d4c754586ff5f84bfc929e2fffdd35c69fbb1
Paul Nzioki <palpaul49@gmail.com>
2023-09-08 23:03:08 +0300

    Update file 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,24 +1,35 @@
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * check_no - checks if character is a number
+ * @c: character to check
+ *
+ * Return: 1 if c is number, 0 otherwise
+ */
 int check_no(char *c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; c[i]; i++)
+	while (c[i])
 	{
 		if (c[i] < '0' || c[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
+/**
+ * print_err - prints error message
+ *
+ * Return: void
+ */
 void print_err(void)
 {
 	int i = 0;
-
 	char err[] = "Error";
-	while (err[i])
+
+	while (err[i] != '\0')
 	{
 		_putchar(err[i]);
 		i++;
@@ -27,64 +38,31 @@ void print_err(void)
 
 	exit(98);
 }
-
+/**
+ * str_len - counts length of a string
+ * @c: the string to get length of
+ *
+ * Return: length of the string
+ */
 int str_len(char *c)
 {
-	int len;
+	int i, len = 0;
 
-	for (len = 0; c[len] != '\0'; len++)
-		;
+	for (i = 0; c[i] != '\0'; i++)
+		len++;
 
 	return (len);
 }
 /**
- *
- *
- *
+ * misc - calculates last part of main
+ * @tlen: total string length
+ * @mem: pointer to memory block
+ * Return: void
  */
-int main(int argc, char *argv[])
+void misc(int tlen, int *mem)
 {
-	char *num1, *num2;
-	int s1len, s2len, tlen, *mem, a, no1, no2, cf, i, j = 0;
+	int i, j = 0;
 
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (argc != 3 || check_no(num1) || check_no(num2))
-		print_err();
-
-	/* get str len*/
-	s1len = str_len(num1);
-	s2len = str_len(num2);
-
-	tlen = s1len + s2len + 1;
-
-	mem = malloc(sizeof(int) * tlen);
-
-	if (mem == NULL)
-		return (1);/* print_err() */
-
-	for (a = 0; a < (s1len + s2len); a++)
-		mem[a] = 0;
-
-	s1len = s1len -  1;
-	while (s1len >= 0)
-	{
-		no1 = num1[s1len] - '0';
-		cf = 0;
-		s2len = s2len - 1;
-		while (s2len >= 0)
-		{
-			no2 = num2[s2len] - '0';
-			cf = cf + mem[s1len + s2len + 1] + (no1 * no2);
-			mem[s1len + s2len + 1] = cf % 10;
-			cf = cf / 10;
-			s2len--;
-		}
-		if (cf > 0)
-			mem[s1len + s2len + 1] += cf;
-		s1len--;
-	}
 	for (i = 0; i < tlen - 1; i++)
 	{
 		if (mem[i])
@@ -96,5 +74,48 @@ int main(int argc, char *argv[])
 		_putchar('0');
 	_putchar('\n');
 	free(mem);
+}
+/**
+ * main - program entry point
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int s1len, s2len, tlen, *mem, a, no1, no2, cf;
+
+	num1 = argv[1];
+	num2 = argv[2];
+	if (argc != 3 || !check_no(num1) || !check_no(num2))
+		print_err();
+	/* get str len*/
+	s1len = str_len(num1);
+	s2len = str_len(num2);
+	tlen = s1len + s2len + 1;
+	mem = malloc(sizeof(int) * tlen);
+	if (mem == NULL)
+		return (1);/* print_err() */
+	if (!mem)
+		return (1);
+	for (a = 0; a < (s1len + s2len); a++)
+		mem[a] = 0;
+	for (s1len = s1len - 1; s1len >= 0; s1len--)
+	{
+		no1 = num1[s1len] - '0';
+		cf = 0;
+		for (s2len = str_len(num2) - 1; s2len >= 0; s2len--)
+		{
+			no2 = num2[s2len] - '0';
+			cf = cf + mem[s1len + s2len + 1] + (no1 * no2);
+			mem[s1len + s2len + 1] = cf % 10;
+			cf = cf / 10;
+		}
+		if (cf > 0)
+			mem[s1len + s2len + 1] += cf;
+	}
+	misc(tlen, mem);
 	return (0);
 }
