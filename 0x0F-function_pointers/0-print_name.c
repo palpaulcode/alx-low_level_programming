4c89b0bb490588478c53c8806a79f710aead3010
Paul Nzioki <palpaul49@gmail.com>
2023-09-13 23:44:32 +0300

    Update file 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints name in uppercase and/or lowercase
  * @name: the name to print
@@ -8,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
