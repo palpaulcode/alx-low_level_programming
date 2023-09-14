7970b90c014303157a64d186d8b4190daee315b5
Paul Nzioki <palpaul49@gmail.com>
2023-09-14 04:43:53 +0300

    Update files 0-print_name.c, function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,11 @@
+#ifndef FUNC_P_H
+#define FUNC_P_H
+
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+#endif /* end FUNC_P_H */
