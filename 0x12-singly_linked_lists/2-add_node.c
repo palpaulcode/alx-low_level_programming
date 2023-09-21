c2258664b5f76366c3d974020b3fa156c8fbb1dc
Paul Nzioki <palpaul49@gmail.com>
2023-09-22 02:44:42 +0300

    Update file 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
