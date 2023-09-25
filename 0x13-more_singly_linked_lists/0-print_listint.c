f4f28644083fcf3d6d9fa3580a7a340dde755f1d
Paul Nzioki <palpaul49@gmail.com>
2023-09-26 00:04:01 +0300

    Add file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *temp;
+
+	temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+# 0x13 C - MOre singly linked lists
+This directory contains solutions to coding tasks from the aforementioned section in the C course within the ALX_SE curriculum
+
+## File `0-print_listint.c`
+This file contains a function that prints all the elements of a `listint_t` list.
+* Prototype: `size_t print_listint(const listint_t *h);`
+* Return: the number of nodes
+
+
