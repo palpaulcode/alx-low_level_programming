a6abad5eefd48fadfc2c31276396a782d204e3c3
Paul Nzioki <palpaul49@gmail.com>
2023-09-26 12:39:27 +0300

    Update file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL)
 		return (NULL);
-
 	temp = *head;
 	/* get number of nodes in the list */
 	while (temp != NULL)
@@ -37,6 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		newnode->next = *head;
 		*head = newnode;
+		return (newnode);
 	}
 	else
 	{
