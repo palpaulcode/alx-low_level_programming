804d955a8c2291949dd8f6b034e6460a4307d4a4
Paul Nzioki <palpaul49@gmail.com>
2023-09-26 12:43:14 +0300

    Update file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0, i = 0;
 	listint_t *temp, *newnode;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	temp = *head;
 	/* get number of nodes in the list */
