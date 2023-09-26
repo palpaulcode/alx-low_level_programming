246a2c7bdc3c75b8655600eabc564f53f4ef0ad1
Paul Nzioki <palpaul49@gmail.com>
2023-09-26 03:29:31 +0300

    Add file 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns node at given index
+ * @head: the head pointer of the linked list
+ * @index: index of node to get starting at 0
+ *
+ * Return: node at given index or NULL if node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0, i = 0;
+	listint_t *node, *temp, *temp2;
+
+	temp = head;
+	temp2 = head;
+
+	/* get length of the list */
+	while (temp2->next != NULL)
+	{
+		temp2 = temp2->next;
+		count++;
+	}
+
+	/* if index is greater than total no. of elements*/
+	if (index > count)
+		return (NULL);
+
+	while (temp != NULL && (i <= index))
+	{
+		node = temp;
+		temp = temp->next;
+		i++;
+	}
+
+	return (node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -34,4 +34,10 @@ This file contains a function that deletes the head node of a `listint_t` linked
 * Prototype: `int pop_listint(listint_t **head);`
 * if the linked list is empty return `0`;
 
+## File `7-get_nodeint.c`
+This file contains a function that returns the nth node of a `listint_t` linked list.
+* Prototype: `listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);`
+* where `index` is the index of the node, starting at `0`
+* if the node does not exist, return `NULL`
+
 
