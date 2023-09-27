5e75da18d02b7de1801c55dfbed4a2bb5e5eaf89
Paul Nzioki <palpaul49@gmail.com>
2023-09-27 12:42:29 +0300

    Update file 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,41 +1,41 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
+ * print_listint_safe - prints a linked list safely
  * @head: head pointer of the linked list
  *
- * Return: number of nodes in the list
+ * Return: no. of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp;
-	size_t count  = 0;
-	long int t_df;
+	const listint_t *slow_ptr;
+	const listint_t *fast_ptr;
+	size_t count = 0;
 
-	if (head == NULL)
+	if (head == NULL || head->next == NULL)
 		exit(98);
 
-	temp = head;
-
-	if (temp == NULL || !temp)
-		exit(98);
-
-	while (temp != NULL)
+	slow_ptr = head;
+	fast_ptr = head;
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		t_df = temp - temp->next;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 		count++;
 
-		if (t_df > 0)
+		if (slow_ptr == fast_ptr)/*loop detected*/
 		{
-			temp = temp->next;
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
-			break;
+			slow_ptr = head;
+			while (slow_ptr != fast_ptr)
+			{
+				slow_ptr = slow_ptr->next;
+				fast_ptr = fast_ptr->next;
+			}
+			printf(" [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			printf("-> [%p] %d\n", (void *)fast_ptr->next, fast_ptr->next->n);
+		 	return (count);
 		}
 	}
-
 	return (count);
 }
